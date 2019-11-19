4f0f029ef6f8840598dc91c96d582685caab5226
MagaliCF <msacfl@outlook.es>
2019-11-18 23:24:19 -0600

    a

--- formulario/vendedor.php
+++ formulario/vendedor.php
@@ -13,55 +13,55 @@
                     <tr>
                         <td>Nombre:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="text" name="nombre"></td>
                     </tr>
                     <tr>
                         <td>Apellido paterno:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="text" name="apellidoP"></td>
                     </tr>
                     <tr>
                         <td>Apellido materno:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="text" name="apellidoM"></td>
                     </tr>
                     <tr>
                         <td>Telefono:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="text" name="telefono"></td>
                     </tr>
                     <tr>
                         <td>Colonia:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="text" name="colonia"></td>
                     </tr>
                     <tr>
                         <td>Calle:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="text" name="calle"></td>
                     </tr>
                     <tr>
                         <td>Numero ext.:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="text" name="numero"></td>
                     </tr>
                     <tr>
                         <td>Alcaldía/municipio:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="text" name="alc_mun"></td>
                     </tr>
                     <tr>
                         <td>Correo:</td>
                         <td>&nbsp; </td>
-                        <td>&nbsp; </td> 
+                        <td>&nbsp; </td>
                         <td><input type="email" name="correo"></td>
                     </tr>
                 </tbody>
